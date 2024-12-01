210915b46d9423a73fbb5f130d30f8a195b9e804
jibsz3 <115227396+jibsz3@users.noreply.github.com>
2024-12-01 14:43:42 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {
-    owner: ['6283811806961@s.whatsapp.net'], // make format nomormu@s.whatsapp.net
+    owner: ['6285230975105@s.whatsapp.net'], // make format nomormu@s.whatsapp.net
 };
 
 module.exports = config;
